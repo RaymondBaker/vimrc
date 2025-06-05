6888c3535e908e7426550a73e45ee1ac71444343
Raymond Baker <raymond.baker@ibm.com>
2025-06-05 16:23:15 -0400

    add neogit

--- lua/package-all/git.lua
+++ lua/package-all/git.lua
@@ -1,32 +1,15 @@
 return {
   {
     "tpope/vim-fugitive"
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
   }
-  --{
-  --  "TimUntersberger/neogit",
-  --  dependencies =
-  --  {
-  --    "nvim-lua/plenary.nvim",
-  --    "sindrets/diffview.nvim",
-  --  },
-  --  opts =
-  --  {
-  --    integrations =
-  --    {
-  --      diffview = true,
-  --    },
-  --  },
-  --  config = function()
-  --    -- TODO: Look into why this doesn't work.
-  --    vim.cmd([[
-  --      "hi def NeogitDiffAddHighlight guibg=#404040 guifg=#859900
-  --      hi! link NeogitDiffAddHighlight GruvboxGreen
-  --      "hi def NeogitDiffDeleteHighlight guibg=#404040 guifg=#dc322f
-  --      hi! link NeogitDiffDeleteHighlight GruvboxRed
-  --      "hi def NeogitDiffContextHighlight guibg=#333333 guifg=#b2b2b2
-  --      "hi def NeogitHunkHeader guifg=#cccccc guibg=#404040
-  --      "hi def NeogitHunkHeaderHighlight guifg=#cccccc guibg=#4d4d4d
-  --    ]])
-  --  end
-  --},
 }
